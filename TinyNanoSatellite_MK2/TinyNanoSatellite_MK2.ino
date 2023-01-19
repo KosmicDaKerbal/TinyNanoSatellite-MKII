d1f0718d9b0396fd68a7147471069d6a2cc4040c
KosmicDaKerbal <88624986+KosmicDaKerbal@users.noreply.github.com>
2023-01-19 08:46:16 +0530

    Update TinyNanoSatellite_MK2.ino

--- TinyNanoSatellite_MK2/TinyNanoSatellite_MK2.ino
+++ TinyNanoSatellite_MK2/TinyNanoSatellite_MK2.ino
@@ -1,16 +1,27 @@
-#include <DFRobot_DHT11.h>
 #include "Transmission.h"
-DFRobot_DHT11 DHT;
+#include <EEPROM.h>
 void setup() {
   pinMode (tx1, OUTPUT); pinMode(tx2, OUTPUT); pinMode(shift, OUTPUT); pinMode(load, OUTPUT);
-  preamble ();
 }
 void loop() {
+  EEPROM.update (0, count);
   DHT.read(Sensor);
   byte T = DHT.temperature;
   byte H = DHT.humidity;
-  transmit (count);
-  transmit (T);
-  transmit (H);
+  transmit (201);  // Sync Code
+  transmit (EEPROM.read (0)); //Power Down Redundancy
+  if (T != 255) {
+    transmit (T);
+  } else {
+    transmit (202); // Error Code 202
+  }
+  if (H != 255) {
+    transmit (H);
+  } else {
+    transmit (203);// Error Code 203
+  }
   count++;
+  if (count > 200) {
+    count = 0;
+  }
 }
